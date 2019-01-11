9e9b8c5e572b21d5da1ea2c63847d0af0f4fe3f7
Saurav <sp.gharti@gmail.com>
2019-01-11 17:47:11 +0000

    initial commit

---
+++ android/app/src/main/java/com/elmedina6crowdboticsc_646/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elmedina6crowdboticsc_646;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "elmedina6crowdboticsc_646";
+    }
+}
